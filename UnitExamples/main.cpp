0987b2d1d94ce814dee631cc07de073783ff153d
Leo Heinsaar <leo.heinsaar@hotmail.com>
2017-03-13 13:38:02 +0400

    Minor (only comments)

--- UnitExamples/main.cpp
+++ UnitExamples/main.cpp
@@ -8,22 +8,22 @@
 
 int main()
 {
-    // Compile-time max.
+    // Compile-time max
     std::cout << "Max<7,2> = " << Max<7, 2>::value << "\n";
     
-    // Or in other way.
+    // Compile-time max another way
     std::cout << "max(10,18) = " << max(10, 18) << "\n";
 
-    // Compile-time factorial.    
+    // Compile-time factorial
     std::cout << "Factorial(7) = " << Factorial<7>::value << "\n";
 
-    // Compile-time fibonacci.
+    // Compile-time fibonacci
     std::cout << "Fibonacci(10) = " << Fibonacci<10>::result << "\n";
 
-    // Compile-time loop.    
+    // Compile-time loop   
     LOOP<6>::EXEC();
 
-    // Compile-time if statement.    
+    // Compile-time if statement
     Queue<int, 10>   q_char;
     Queue<int, 1000> q_int;
     std::cout << sizeof(q_char) << " " << sizeof(q_int) << '\n';
